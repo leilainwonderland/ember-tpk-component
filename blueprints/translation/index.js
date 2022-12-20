eefb3dd45091b66dbd39cdb0357889b8db2403af
leilainwonderland <leila.nunez98@gmail.com>
2022-12-20 01:23:17 +0100

    translation blueprints

---
+++ blueprints/translation/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  description: 'help to create the structure of translation files',
+
+  availableOptions: [
+    {
+      name: 'language',
+      type: String,
+      default: 'fr',
+      aliases: ['l'],
+    },
+    {
+      name: 'pod',
+      type: Boolean,
+      default: true,
+      aliases: ['pod'],
+    },
+  ],
+
+  locals(options) {
+    return {
+      language: options.language,
+    };
+  },
+
+  fileMapTokens(options) {
+    return {
+      __lang__(options) {
+        return `${options.locals.language}-${options.locals.language}`;
+      },
+    };
+  },
+};
